bb7ccaac398d4485091e4fbeb897c3d53b3af415
Ruoxi Wang <thuwrx10@gmail.com>
2017-09-24 22:34:27 +0800

    Use localcache in find haul route routine and bug fixes.

--- find-haul-route.py
+++ find-haul-route.py
@@ -1,9 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import pickle
+import localcache
 
-# cargoVolumeLim = 0
-# budgetLim = 0
+cargoVolumeLim = 400
+budgetLim = 15e6
+highsecOnly = True
 minMargin = 0.5
 minProfit = 1e6
 
@@ -22,11 +24,20 @@ with open('./cache/ordersAllTypeIdSorted.bin', 'rb') as inputFile:
     orders = pickle.load(inputFile)
     pass
 
-print('Loading systems info...')
-with open('./cache/systemsInfo.bin', 'rb') as inputFile:
-    print("Opened '{}'".format(inputFile.name))
-    systems = pickle.load(inputFile)
-    pass
+items = localcache.Cache('item', 
+        'https://esi.tech.ccp.is/latest/universe/types/',
+        '/?datasource=tranquility&language=en-us')
+systems = localcache.Cache('system', 
+        'https://esi.tech.ccp.is/latest/universe/systems/',
+        '/?datasource=tranquility&language=en-us')
+stations = localcache.Cache('station',
+        'https://esi.tech.ccp.is/latest/universe/stations/',
+        '/?datasource=tranquility')
+# print('Loading systems info...')
+# with open('./cache/systemsInfo.bin', 'rb') as inputFile:
+#     print("Opened '{}'".format(inputFile.name))
+#     systems = pickle.load(inputFile)
+#     pass
 
 # print('Loading items info...')
 # with open('./cache/itemsInfo.bin', 'rb') as inputFile:
@@ -60,10 +71,40 @@ for typeId in orders:
         for buyOrder in orders[typeId][buy]:
             if sellOrder[price] > buyOrder[price]:
                 continue
-            
+
             availVolume = min(sellOrder[volume], buyOrder[volume])
             profit = availVolume * (buyOrder[price] - sellOrder[price])
             margin = buyOrder[price] / sellOrder[price] - 1
+            if margin < minMargin:
+                continue
+            if profit < minProfit:
+                continue
+            if buyOrder[price] > budgetLim:
+                continue
+            try:
+                if cargoVolumeLim != 0:
+                    itemVolume = items.get(typeId)['volume']
+                    if itemVolume > cargoVolumeLim:
+                        continue
+                    availVolume = min(availVolume, cargoVolumeLim // itemVolume)
+                    profit = availVolume * (buyOrder[price] - sellOrder[price])
+                    if profit < minProfit:
+                        continue
+                    pass
+                if highsecOnly:
+                    if systems.get(
+                            stations.get(buyOrder['location_id'])['system_id']
+                            )[security] < 0.5:
+                        continue
+                    if systems.get(
+                            stations.get(sellOrder['location_id'])['system_id']
+                            )[security] < 0.5:
+                        continue
+                    pass
+                pass
+            except Exception as errorMessage:
+                print(errorMessage.args[0])
+                continue
             legitPair = dict([
                 ('volume', availVolume),
                 ('profit', profit),
@@ -71,13 +112,14 @@ for typeId in orders:
                 ('buy', buyOrder),
                 ('sell', sellOrder)
                 ])
-            if margin < minMargin:
-                continue
-            if profit < minProfit:
-                continue
             tradePairs.append(legitPair)
-            print('TypeID: {}, Profit: {}, Margin: {}.'.
-                    format(typeId, profit, margin))
+            with open('./result.txt', 'a') as outputText:
+                outputText.write('Item: {}, Profit: {:,.2f}ISK, Margin: {:.2%}, From: {}, To: {}.\n'.
+                        format(items.get(typeId)['name'], profit, margin,
+                            stations.get(sellOrder['location_id'])['name'],
+                            stations.get(buyOrder['location_id'])['name']
+                            ))
+                pass
             pass
         pass
     pass
--- localcache.py
+++ localcache.py
@@ -14,27 +14,34 @@ class Cache:
             os.makedirs(folder)
             pass
         
-        try:
-            self.filePath = self.folder + self.name + self.fileSuffix
+        self.filePath = self.folder + self.name + self.fileSuffix
+        if os.path.exists(self.filePath):
             print("Loading data from '{}'...".format(self.filePath))
             with open(self.filePath, 'rb') as self.file:
                 self.data = pickle.load(self.file)
                 print('Completed.')
                 pass
             pass
-
-        except FileNotFoundError:
+        else:
             print('File not found, will create a new one.')
             self.data = dict()
             pass
 
     def get(self, ID):
+        ID = str(ID)
+        if self.data.get(ID) == []:
+            raise Exception('ID previously not found on server.')
         if self.data.get(ID) == None:
             print('{} {} not found in local cache, retriving from server...'.
                     format(self.name.capitalize(), ID))
             req = requests.get(self.prefix + ID + self.suffix)
-            assert req.status_code == 200
+            if req.status_code != 200:
+                print(str(req.status_code) + ': ' + req.json['error'])
+                self.data[ID] = []
+                self.save()
+                raise Exception(str(req.status_code) + ': ' + req.json['error'])
             self.data[ID] = req.json()
+            self.save()
             if req.json().get('name') != None:
                 print('{} {} is "{}", saved to local cache.'.
                         format(self.name.capitalize(), ID, req.json()['name']))
