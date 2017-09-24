79eac236fadf82d65b4b5208f75ef6d5c3d323cd
Ruoxi Wang <thuwrx10@gmail.com>
2017-09-25 06:52:51 +0800

    High-sec and low-sec route query and minimum cost check.

--- find-haul-route.py
+++ find-haul-route.py
@@ -4,10 +4,10 @@ import pickle
 import localcache
 
 cargoVolumeLim = 4000
-budgetLim = 36e6
-highsecOnly = True
+budgetLim = 300e6
+highsecOnly = True 
 minMargin = 0.5
-minProfit = 5e6
+minProfit = 3e6
 
 name = 'name'
 price = 'price'
@@ -33,6 +33,15 @@ systems = localcache.Cache('system',
 stations = localcache.Cache('station',
         'https://esi.tech.ccp.is/latest/universe/stations/',
         '/?datasource=tranquility')
+if highsecOnly:
+    routes = localcache.Cache('routehigh',
+            'https://esi.tech.ccp.is/latest/route/',
+            '/?datasource=tranquility&flag=secure')
+else:
+    routes = localcache.Cache('route',
+            'https://esi.tech.ccp.is/latest/route/',
+            '/?datasource=tranquility&flag=shortest')
+
 # print('Loading systems info...')
 # with open('./cache/systemsInfo.bin', 'rb') as inputFile:
 #     print("Opened '{}'".format(inputFile.name))
@@ -81,6 +90,24 @@ def totalISK(ordersList, volume):
     # print('Total amount: {:,.2f} ISK.'.format(amount))
     return amount
 
+def getRoute(src, dst):
+    if src == dst:
+        return [src]
+    if src < dst:
+        routeId = str(src) + '/' + str(dst)
+    else:
+        routeId = str(dst) + '/' + str(src)
+    try:
+        route = routes.get(routeId)
+    except Exception as errorMessage:
+        print(errorMessage.args[0])
+        return []
+
+    if src < dst:
+        return route
+    else:
+        return list(reversed(route))
+
 tradePairs = []
 for typeId in orders:
 #     if len(orders[typeId][sell]) == 0:
@@ -108,14 +135,19 @@ for typeId in orders:
             profit = buyTotal - sellTotal
             margin = buyOrdersList[0][price] / sellOrdersList[0][price] - 1
             if margin < minMargin:
-                print('Margin = {:.2%} < {:.2%}, too low.'.format(margin, minMargin))
+                # print('Margin = {:.2%} < {:.2%}, too low.'.format(margin, minMargin))
                 continue
             if profit < minProfit:
-                print('Profit = {:,.2f} ISK < {:,.2f} ISK, too low.'.format(profit, minProfit))
+                # print('Profit = {:,.2f} ISK < {:,.2f} ISK, too low.'.format(profit, minProfit))
                 continue
-            if sellOrdersList[0][price] > budgetLim:
-                print('Price = {:,.2f} ISK > {:,.2f} ISK, too high.'.\
-                        format(sellOrdersList[0][price], budgetLim))
+            buyerMinVolumes = [order['min_volume'] for order in buyOrdersList]
+            minVolume = min(buyerMinVolumes)
+            if minVolume > totalVolume(sellOrdersList):
+                continue
+            minCost = totalISK(sellOrdersList, minVolume)
+            if minCost > budgetLim:
+                print('Min. cost = {:,.2f} ISK > {:,.2f} ISK, too high.'.\
+                        format(minCost, budgetLim))
                 continue
             try:
                 if cargoVolumeLim != 0:
@@ -146,7 +178,7 @@ for typeId in orders:
 
                 # Check updated profit
                 if profit < minProfit:
-                    print('Profit = {:,.2f} ISK < {:,.2f} ISK, too low.'.format(profit, minProfit))
+                    # print('Profit = {:,.2f} ISK < {:,.2f} ISK, too low.'.format(profit, minProfit))
                     continue
                 # Actual margin, can only be lower.
                 marginActual = buyTotal / sellTotal - 1
@@ -154,23 +186,27 @@ for typeId in orders:
                     if systems.get(
                             stations.get(locBuy)['system_id']
                             )[security] < 0.5:
-                        print('Buyer location low-sec.')
+                        # print('Buyer location low-sec.')
                         continue
                     if systems.get(
                             stations.get(locSell)['system_id']
                             )[security] < 0.5:
-                        print('Seller location low-sec.')
+                        # print('Seller location low-sec.')
                         continue
                     pass
+                jumps = len(getRoute(stations.get(locSell)['system_id'],
+                    stations.get(locBuy)['system_id'])) - 1
                 pass
             except Exception as errorMessage:
                 print(errorMessage.args[0])
                 continue
             legitPair = dict([
                 ('volume', availVolume),
+                ('min_volume', minVolume),
                 ('profit', profit),
                 ('margin', margin),
                 ('margin_actual', marginActual),
+                ('jumps', jumps),
                 ('buy', buyOrdersList),
                 ('sell', sellOrdersList)
                 ])
@@ -179,7 +215,11 @@ for typeId in orders:
                 outputText.write('Item: \t{} \nProfit: \t{:,.2f} ISK \nMargin: \t{:.2%}/{:.2%} \n'.
                         format(items.get(typeId)['name'], profit, margin, marginActual) + 
                         'Volume: \t{} \nCost: \t{:,.2f} ISK \n'.format(availVolume, sellTotal) + 
-                        'From: \t{} \n To: \t{} \n'.format(stations.get(locSell)['name'], stations.get(locBuy)['name']) + 
+                        'Min. volume: \t{} \n'.format(minVolume) +
+                        'Min. cost: \t{:,.2f} ISK \n'.format(minCost) +
+                        'From: \t{} \n'.format(stations.get(locSell)['name']) + 
+                        'To: \t{} \n'.format(stations.get(locBuy)['name']) + 
+                        'Jumps: \t{} \n'.format(jumps) + 
                         'Profit limit factor: \t{}.\n'.format(profitLimFactor) + '\n')
                 pass
             pass
