fc9bbb4ebd31c754e8c01d541d55d8224386af83
Ruoxi Wang <thuwrx10@gmail.com>
2017-10-03 18:37:03 +0800

    Rename tables after inserting completes.

--- syncmarket.py
+++ syncmarket.py
@@ -23,12 +23,12 @@ def execSQL(sql, conn):
     conn.execute(sql)
 
 
-def initDB():
-    logger.debug("Connecting to '{}'...".format(dbPath))
-    conn = sqlite3.connect(dbPath)
-    execSQL("DROP TABLE IF EXISTS buyOrderInserting;", conn)
+def initDB(pconn):
+    # logger.debug("Connecting to '{}'...".format(dbPath))
+    # conn = sqlite3.connect(dbPath)
+    execSQL("DROP TABLE IF EXISTS buyOrdersInserting;", pconn)
     execSQL('\n'.join([
-        "CREATE TABLE buyOrderInserting (",
+        "CREATE TABLE buyOrdersInserting (",
         "order_id INTEGER PRIMARY KEY,",
         "type_id INTEGER NOT NULL,",
         "location_id INTEGER NOT NULL,",
@@ -40,10 +40,10 @@ def initDB():
         "range TEXT NOT NULL CHECK (range IN ('station', 'region', ",
         "'solarsystem', '1', '2', '3', '4', '5', '10', '20', '30', '40')),",
         "duration INTEGER NOT NULL CHECK (duration > 0),",
-        "issued INTEGER NOT NULL);"]), conn)
-    execSQL("DROP TABLE IF EXISTS sellOrderInserting;", conn)
+        "issued INTEGER NOT NULL);"]), pconn)
+    execSQL("DROP TABLE IF EXISTS sellOrdersInserting;", pconn)
     execSQL('\n'.join([
-        "CREATE TABLE sellOrderInserting (",
+        "CREATE TABLE sellOrdersInserting (",
         "order_id INTEGER PRIMARY KEY,",
         "type_id INTEGER NOT NULL,",
         "location_id INTEGER NOT NULL,",
@@ -56,9 +56,9 @@ def initDB():
         # "volume_remain INTEGER NOT NULL,",
         # "price REAL NOT NULL,",
         # "duration INTEGER NOT NULL,",
-        "issued INTEGER NOT NULL);"]), conn)
-    conn.commit()
-    conn.close()
+        "issued INTEGER NOT NULL);"]), pconn)
+    # conn.commit()
+    # conn.close()
 
 
 def buyOrderTuple(order, reg):
@@ -115,20 +115,20 @@ def insertDB(ordersList, conn, reg):
                             sellTupleList,
                             reg)
         if(buyTupleList):
-            execSQLMany("INSERT OR IGNORE INTO buyOrderInserting VALUES "
+            execSQLMany("INSERT OR IGNORE INTO buyOrdersInserting VALUES "
                         "({});".format(','.join(11*'?')),
                         conn, buyTupleList)
         if(sellTupleList):
-            execSQLMany("INSERT OR IGNORE INTO sellOrderInserting VALUES "
+            execSQLMany("INSERT OR IGNORE INTO sellOrdersInserting VALUES "
                         "({});".format(','.join(9*'?')),
                         conn, sellTupleList)
         # for order in ordersList:
         #     if order['is_buy_order']:
-        #         execSQL("INSERT INTO buyOrderInserting VALUES "
+        #         execSQL("INSERT INTO buyOrdersInserting VALUES "
         #                 "({});".format(','.join(11*'?')),
         #                 conn, data=buyOrderTuple(order, reg))
         #     else:
-        #         execSQL("INSERT INTO sellOrderInserting VALUES "
+        #         execSQL("INSERT INTO sellOrdersInserting VALUES "
         #                 "({});".format(','.join(10*'?')),
         #                 conn, data=sellOrderTuple(order, reg))
         #     pass
@@ -136,29 +136,29 @@ def insertDB(ordersList, conn, reg):
     pass
 
 
-def getOrders(ordersList, reg, page):
+def getOrders(pordersList, preg, ppage):
     req = requests.get('https://esi.tech.ccp.is/latest/markets/' +
-                       str(reg) + '/orders/?datasource=tranquility' +
-                       '&order_type=all&page=' + str(page))
+                       str(preg) + '/orders/?datasource=tranquility' +
+                       '&order_type=all&page=' + str(ppage))
     assert req.status_code == 200
-    ordersList.extend(req.json())
+    pordersList.extend(req.json())
     # insertDB(req.json(), int(reg))
-    logger.info('Region {} Page {} received.'.format(regionNames[reg], page))
+    logger.info('Region {} Page {} received.'.format(regionNames[preg], ppage))
     pass
 
 
-def getFirstPage(ordersDict, pageCountsDict, reg):
+def getFirstPage(porders, ppageCounts, preg):
     logger.info('Getting the first page of orders '
-                'in region {}'.format(regionNames[reg]))
+                'in region {}'.format(regionNames[preg]))
     req = requests.get('https://esi.tech.ccp.is/latest/markets/' +
-                       reg + '/orders/?datasource=tranquility' +
+                       preg + '/orders/?datasource=tranquility' +
                        '&order_type=all&page=1')
     assert req.status_code == 200
     # return req.json(), int(req.headers['x-pages'])
-    ordersDict[reg] = req.json()
-    pageCountsDict[reg] = int(req.headers['x-pages'])
+    porders[preg] = req.json()
+    ppageCounts[preg] = int(req.headers['x-pages'])
     logger.info('Region {} has {} '
-                'pages'.format(regionNames[reg], pageCountsDict[reg]))
+                'pages'.format(regionNames[preg], ppageCounts[preg]))
     # insertDB(req.json(), int(reg))
 
 
@@ -170,35 +170,47 @@ def getRegionList():
     return req.json()
 
 
-def fetchMarketData():
+def fetchMarketData(porders, pregionsStr):
+    pageCounts = dict.fromkeys(pregionsStr, 0)
     with ThreadPoolExecutor(max_workers=10) as executor:
-        for reg in regionsStr:
-            executor.submit(getFirstPage, orders, pageCounts, reg)
+        for reg in pregionsStr:
+            executor.submit(getFirstPage, porders, pageCounts, reg)
             pass
 
     with ThreadPoolExecutor(max_workers=10) as executor:
-        for reg in regionsStr:
+        for reg in pregionsStr:
             try:
                 assert pageCounts[reg] >= 1
             except AssertionError:
                 import sys
                 sys.exit("{}, {}".format(reg, pageCounts[reg]))
             for page in range(1, pageCounts[reg]):
-                executor.submit(getOrders, orders[reg], reg, page+1)
+                executor.submit(getOrders, porders[reg], reg, page+1)
                 pass
             pass
         pass
 
 
+def replaceTable(pconn):
+    sql = """DROP TABLE IF EXISTS buyOrders;
+ALTER TABLE buyOrdersInserting RENAME TO buyOrders;
+DROP TABLE IF EXISTS sellOrders;
+ALTER TABLE sellOrdersInserting RENAME TO sellOrders;"""
+    logger.debug("Executing SQL:\n" + sql)
+    pconn.executescript(sql)
+
+
 def dumpToDatabse():
     logger.debug("Connecting to '{}'...".format(dbPath))
     conn = sqlite3.connect(dbPath)
+    initDB(conn)
     for reg in regionsStr:
         logger.info('Region {} has {} '
                     'orders, inserting into '
                     'database'.format(regionNames[reg], len(orders[reg])))
         insertDB(orders[reg], conn, int(reg))
         pass
+    replaceTable(conn)
     conn.commit()
     conn.close()
 
@@ -206,10 +218,6 @@ def dumpToDatabse():
 logger = logging.getLogger()
 if __name__ == '__main__':
     logger.setLevel(logging.DEBUG)
-
-initDB()
-
-if __name__ == '__main__':
     regionsInt = getRegionList()
 
     regionsStr = []
@@ -217,11 +225,10 @@ if __name__ == '__main__':
         regionsStr.append(str(reg))
         pass
     orders = dict.fromkeys(regionsStr, [])
-    pageCounts = dict.fromkeys(regionsStr, 0)
     regionNames = dict.fromkeys(regionsStr, None)
 
     for reg in regionsInt:
         regionNames[str(reg)] = sde.getItemName(reg)
 
-    fetchMarketData()
+    fetchMarketData(orders, regionsStr)
     dumpToDatabse()
