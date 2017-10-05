25a1e913a3d4b86acbcb35d2e7efa8b663490c05
Ruoxi Wang <thuwrx10@gmail.com>
2017-10-05 23:56:09 +0800

    Making pymode happy.

--- sde.py
+++ sde.py
@@ -4,6 +4,7 @@ import sqlite3
 import logging
 # from syncmarket import dbPath as marketDBPath
 import os
+import sqlqueries
 
 
 try:
@@ -18,133 +19,118 @@ logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 
-def _get(id, idcol, table, cols):
-    assert isinstance(id, int)
-    assert isinstance(idcol, str)
-    assert isinstance(cols, str)
-    assert isinstance(table, str)
-    c = _conn.cursor()
-    c.execute("SELECT {} FROM {} "
-              "WHERE {}=?".format(cols, table, idcol), (id,))
-    assert c.arraysize == 1
-    return c.fetchone()
+class Database:
+    def _get(self, id, idcol, table, cols):
+        assert isinstance(id, int)
+        assert isinstance(idcol, str)
+        assert isinstance(cols, str)
+        assert isinstance(table, str)
+        c = self._conn.execute("SELECT {} FROM {} "
+                               "WHERE {}=?".format(cols, table, idcol), (id,))
+        assert c.arraysize == 1
+        return c.fetchone()
 
+    def _gethdd(self, id, idcol, table, cols):
+        assert isinstance(id, int)
+        assert isinstance(idcol, str)
+        assert isinstance(cols, str)
+        assert isinstance(table, str)
+        c = self._conn.execute("SELECT {} FROM hdd.{} "
+                               "WHERE {}=?".format(cols, table, idcol), (id,))
+        assert c.arraysize == 1
+        return c.fetchone()
 
-def _gethdd(id, idcol, table, cols):
-    assert isinstance(id, int)
-    assert isinstance(idcol, str)
-    assert isinstance(cols, str)
-    assert isinstance(table, str)
-    c = _conn.cursor()
-    c.execute("SELECT {} FROM hdd.{} "
-              "WHERE {}=?".format(cols, table, idcol), (id,))
-    assert c.arraysize == 1
-    return c.fetchone()
+    def cacheTableToMemory(self, table):
+        c = self._conn.execute("SELECT sql FROM hdd.sqlite_master "
+                               "WHERE name = ? AND type = 'table';", (table,))
+        sql = c.fetchone()[0]
+        c.execute(sql)
+        c.execute("INSERT INTO main.{0} SELECT * FROM hdd.{0};".format(table))
+        c.close()
+        pass
 
+    def _getStation(self, stationID, cols):
+        return self._get(stationID,
+                         idcol='stationID',
+                         table='staStations',
+                         cols=cols)
 
-def cacheTableToMemory(table):
-    c = _conn.cursor()
-    c.execute("SELECT sql FROM hdd.sqlite_master "
-              "WHERE name = ? AND type = 'table';", (table,))
-    sql = c.fetchone()[0]
-    c.execute(sql)
-    c.execute("INSERT INTO main.{0} SELECT * FROM hdd.{0};".format(table))
-    c.close()
-    pass
+    def getStationSecurity(self, stationID):
+        return self._getStation(stationID, cols='security')[0]
 
+    def getStationSolarSystem(self, stationID):
+        return self._getStation(stationID, cols='solarSystemID')[0]
 
-def _getStation(stationID, cols):
-    return _get(stationID,
-                idcol='stationID',
-                table='staStations',
-                cols=cols)
+    def getStationConstellation(self, stationID):
+        return self._getStation(stationID, cols='constellationID')[0]
 
+    def getStationRegion(self, stationID):
+        return self._getStation(stationID, cols='regionID')[0]
 
-def getStationSecurity(stationID):
-    return _getStation(stationID, cols='security')[0]
+    def _cacheItemsPackVols(self):
+        c = self._conn.execute("SELECT typeID, volume FROM hdd.invVolumes;")
+        res = c.fetchall()
+        logger.info("Found {} item types "
+                    "which has packaged volume.".format(len(res)))
+        self.typesPackVol = {str(t[0]): t[1] for t in res}
 
+    def _getType(self, typeID, cols):
+        return self._get(typeID,
+                         idcol='typeID',
+                         table='invTypes',
+                         cols=cols)
+        pass
 
-def getStationSolarSystem(stationID):
-    return _getStation(stationID, cols='solarSystemID')[0]
+    def getTypeVolume(self, typeID):
+        assert isinstance(typeID, int)
+        vol = self.typesPackVol.get(str(typeID))
+        if vol:
+            return vol
+        else:
+            return self._getType(typeID,
+                                 cols='volume')[0]
 
+    def getTypeName(self, typeID):
+        return self._getType(typeID,
+                             cols='typeName')[0]
 
-def getStationConstellation(stationID):
-    return _getStation(stationID, cols='constellationID')[0]
+    def getItemName(self, itemID):
+        return self._get(itemID,
+                         idcol='itemID',
+                         table='invNames',
+                         cols='itemName')[0]
 
+    def _getSolarSystem(self, solarSystemID, cols):
+        return self._get(solarSystemID,
+                         idcol='solarSystemID',
+                         table='mapSolarSystems',
+                         cols=cols)
 
-def getStationRegion(stationID):
-    return _getStation(stationID, cols='regionID')[0]
+    def getSolarSystemSecurity(self, solarSystemID):
+        return self._getSolarSystem(solarSystemID,
+                                    cols='security')[0]
 
+    def __init__(self):
+        self._conn = sqlite3.connect(':memory:')
+        self._conn.execute("ATTACH './db/sde.sqlite' AS hdd;")
 
-def _cacheItemsPackVols():
-    c = _conn.cursor()
-    c.execute("SELECT typeID, volume FROM hdd.invVolumes;")
-    res = c.fetchall()
-    logger.info("Found {} item types "
-                "which has packaged volume.".format(len(res)))
-    dict = {str(t[0]): t[1] for t in res}
-    return dict
+        self.cacheTableToMemory('invTypes')
+        self.cacheTableToMemory('invNames')
+        self.cacheTableToMemory('staStations')
+        self.cacheTableToMemory('mapSolarSystems')
+        self._cacheItemsPackVols()
 
+        self._conn.execute("DETACH hdd;")
 
-def _getType(typeID, cols):
-    return _get(typeID,
-                idcol='typeID',
-                table='invTypes',
-                cols=cols)
-    pass
+        marketDBPath = './db/market.sqlite'
+        if os.path.exists(marketDBPath):
+            self._conn.execute("ATTACH '{}' AS hdd;".format(marketDBPath))
 
+            self.cacheTableToMemory('buyOrders')
+            self.cacheTableToMemory('sellOrders')
+            self.cacheTableToMemory('publicStructures')
 
-def getTypeVolume(typeID):
-    assert isinstance(typeID, int)
-    vol = typesPackVol.get(str(typeID))
-    if vol:
-        return vol
-    else:
-        return _getType(typeID,
-                        cols='volume')[0]
+            self._conn.execute("DETACH hdd;")
 
-
-def getTypeName(typeID):
-    return _getType(typeID,
-                    cols='typeName')[0]
-
-
-def getItemName(itemID):
-    return _get(itemID,
-                idcol='itemID',
-                table='invNames',
-                cols='itemName')[0]
-
-
-def _getSolarSystem(solarSystemID, cols):
-    return _get(solarSystemID,
-                idcol='solarSystemID',
-                table='mapSolarSystems',
-                cols=cols)
-
-
-def getSolarSystemSecurity(solarSystemID):
-    return _getSolarSystem(solarSystemID,
-                           cols='security')[0]
-
-
-_conn = sqlite3.connect(':memory:')
-_conn.execute("ATTACH './db/sde.sqlite' AS hdd;")
-
-cacheTableToMemory('invTypes')
-cacheTableToMemory('invNames')
-cacheTableToMemory('staStations')
-cacheTableToMemory('mapSolarSystems')
-typesPackVol = _cacheItemsPackVols()
-
-_conn.execute("DETACH hdd;")
-
-marketDBPath = './db/market.sqlite'
-if os.path.exists(marketDBPath):
-    _conn.execute("ATTACH '{}' AS hdd;".format(marketDBPath))
-
-    cacheTableToMemory('buyOrders')
-    cacheTableToMemory('sellOrders')
-
-    _conn.execute("DETACH hdd;")
-    pass
+            self._conn.execute(sqlqueries.hiSecMarketsView)
+            pass
