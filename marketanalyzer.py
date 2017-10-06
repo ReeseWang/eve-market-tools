b5a24adb1e37b2b5bcb23ed461051a413c0c194b
Ruoxi Wang <thuwrx10@gmail.com>
2017-10-07 02:30:05 +0800

    MarketAnalyzer class.

--- marketanalyzer.py
+++ marketanalyzer.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import logging
 import sqlqueries
 from sde import Database
@@ -19,84 +21,90 @@ def rangeString(r):
             return r.capitalize()
 
 
-def printSellerList(typeID):
-    assert isinstance(typeID, int)
-    name = db.getTypeName(typeID)
-    c = db.execSQL(sqlqueries.listSellOrders(), data=(typeID,))
-    print(name.upper(), 'SELLER SUMMARY:\n')
-    print(tabulate(
-        [
+class MarketAnalyzer:
+    def printSellerList(self, typeID):
+        assert isinstance(typeID, int)
+        name = self.db.getTypeName(typeID)
+        c = self.db.execSQL(sqlqueries.listSellOrders(), data=(typeID,))
+        print(name.upper(), 'SELLER SUMMARY:\n')
+        print(tabulate(
             [
-                '{:.1f} '.format(e[0]) +
-                ' - '.join(
-                    [
-                        e[1],  # Region name
-                        # e[2],  # Constellation name
-                        e[3][0:50]  # Station name, solar system name inc.
-                    ]
-                ),
-                float(e[4]),  # Volume remain
-                e[5],  # Price
-                e[4]*e[5]  # Sum
-            ] for e in c.fetchall()
-        ],
-        [
+                [
+                    '{:.1f} '.format(e[0]) +
+                    ' - '.join(
+                        [
+                            e[1],  # Region name
+                            # e[2],  # Constellation name
+                            e[3][0:50]  # Station name, solar system name inc.
+                        ]
+                    ),
+                    float(e[4]),  # Volume remain
+                    e[5],  # Price
+                    e[4]*e[5]  # Sum
+                ] for e in c.fetchall()
+            ],
+            self.sellListHeaders,
+            tablefmt='pipe',
+            numalign='right',
+            floatfmt=('', ',.0f', ',.2f', ',.2f')
+        ))
+
+    def printBuyerList(self, typeID):
+        assert isinstance(typeID, int)
+        name = self.db.getTypeName(typeID)
+        c = self.db.execSQL(sqlqueries.listBuyOrders(), data=(typeID,))
+        print(name.upper(), 'BUYER SUMMARY:\n')
+        print(tabulate(
+            [
+                [
+                    '{:.1f} '.format(e[0]) +
+                    ' - '.join(
+                        [
+                            e[1],  # Region name
+                            # e[2],4 # Constellation name
+                            e[3][0:40]  # Station name, solar system name inc.
+                        ]
+                    ),
+                    rangeString(e[7]),  # Range
+                    float(e[4]),  # Volume remain
+                    float(e[6]),  # Min volume
+                    e[5],  # Price
+                    e[4]*e[5]  # Sum
+                ] for e in c.fetchall()
+            ],
+            self.buyListHeaders,
+            tablefmt='pipe',
+            numalign='right',
+            floatfmt=('', '', ',.0f', ',.0f', ',.2f', ',.2f')
+        ))
+
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+        self.sellListHeaders = [
             'Location',
             'Remaining',
             'Ask price / ISK',
             'Sum / ISK'
-        ],
-        tablefmt='pipe',
-        numalign='right',
-        floatfmt=('', ',.0f', ',.2f', ',.2f')
-    ))
-
-
-def printBuyerList(typeID):
-    assert isinstance(typeID, int)
-    name = db.getTypeName(typeID)
-    c = db.execSQL(sqlqueries.listBuyOrders(), data=(typeID,))
-    print(name.upper(), 'BUYER SUMMARY:\n')
-    print(tabulate(
-        [
-            [
-                '{:.1f} '.format(e[0]) +
-                ' - '.join(
-                    [
-                        e[1],  # Region name
-                        # e[2],4 # Constellation name
-                        e[3][0:40]  # Station name, solar system name inc.
-                    ]
-                ),
-                rangeString(e[7]),  # Range
-                float(e[4]),  # Volume remain
-                float(e[6]),  # Min volume
-                e[5],  # Price
-                e[4]*e[5]  # Sum
-            ] for e in c.fetchall()
-        ],
-        [
+        ]
+        self.buyListHeaders = [
             'Location',
             'Range',
             'Remaining',
             'Min volume',
             'Bid price / ISK',
             'Sum / ISK'
-        ],
-        tablefmt='pipe',
-        numalign='right',
-        floatfmt=('', '', ',.0f', ',.0f', ',.2f', ',.2f')
-    ))
+        ]
+        self.db = Database()
+        self.db.execSQLScript(sqlqueries.createSecFilteredMarketsView(0.45, 1))
+        self.db.execSQLScript(sqlqueries.createSecFilteredOrdersView())
 
 
 if __name__ == '__main__':
     logger = logging.getLogger(__name__)
     channel = logging.StreamHandler()
     channel.setFormatter(LogFormatter())
-    logging.basicConfig(handlers=[channel], level=logging.DEBUG)
+    logging.basicConfig(handlers=[channel], level=logging.WARNING)
 
-    db = Database()
-    db.execSQLScript(sqlqueries.createSecFilteredMarketsView(0.45, 1))
-    db.execSQLScript(sqlqueries.createSecFilteredOrdersView())
-    printSellerList(6677)
-    printBuyerList(34)
+    analyzer = MarketAnalyzer()
+    analyzer.printSellerList(6677)
+    analyzer.printBuyerList(34)
