5de5fea012e920c8b9a878a2ef20200e69c57e8e
Ruoxi Wang <thuwrx10@gmail.com>
2017-10-06 18:14:23 +0800

    Tab -> Space to make pymode happy.

--- sqlqueries.py
+++ sqlqueries.py
@@ -2,31 +2,58 @@ hiSecMarketsView = '''
 CREATE TEMP VIEW IF NOT EXISTS hiSecMarkets
 AS
 SELECT
-  structure_id AS stationID,
-  publicStructures.name AS stationName,
-  solar_system_id as solarSystemId,
-  publicStructures.x AS x,
-  publicStructures.y AS y,
-  publicStructures.z AS z,
-  mapSolarSystems.constellationID AS constellationID
+    structure_id AS stationID,
+    publicStructures.name AS stationName,
+    solar_system_id as solarSystemId,
+    publicStructures.x AS x,
+    publicStructures.y AS y,
+    publicStructures.z AS z,
+    mapSolarSystems.constellationID AS constellationID,
+    mapSolarSystems.security AS security
 FROM
-  publicStructures
+    publicStructures
 INNER JOIN mapSolarSystems ON
-  mapSolarSystems.solarSystemId = publicStructures.solar_system_id
+    mapSolarSystems.solarSystemId = publicStructures.solar_system_id
 WHERE
-  mapSolarSystems.security > 0.45
+    mapSolarSystems.security > 0.45
 UNION ALL
 SELECT
-  stationID,
-  stationName,
-  solarSystemId,
-  x,
-  y,
-  z,
-  constellationID
+    stationID,
+    stationName,
+    solarSystemID,
+    x,
+    y,
+    z,
+    constellationID,
+    security
 FROM
-  staStations
+    staStations
 WHERE
-  security > 0.45
+    security > 0.45
+;
+'''
+hiSecBuyOrdersView = '''
+CREATE TEMP VIEW IF NOT EXISTS hiSecBuyOrders
+AS
+SELECT
+    order_id,
+    type_id,
+    location_id,
+    region_id,
+    volume_total,
+    volume_remain,
+    min_volume,
+    price,
+    range,
+    duration,
+    issued,
+    hiSecMarkets.stationName AS stationName,
+    hiSecMarkets.solarSystemID AS solarSystemID,
+    hiSecMarkets.constellationID AS constellationID,
+    hiSecMarkets.security AS security
+FROM
+    buyOrders
+INNER JOIN hiSecMarkets ON
+    hiSecMarkets.stationID = buyOrders.location_id
 ;
 '''
