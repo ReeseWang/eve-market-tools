46dfe5f85485638a01f58356d10973a5b7e369e7
Ruoxi Wang <thuwrx10@gmail.com>
2017-10-09 23:36:24 +0800

    SDE Download: Overwrite the file if exists.

--- download-sde-database-sqlite.sh
+++ download-sde-database-sqlite.sh
@@ -7,5 +7,5 @@ echo "Getting the SQLite conversion of the latest SDE..."
 curl https://www.fuzzwork.co.uk/dump/sqlite-latest.sqlite.bz2 \
 	--create-dirs -o ./db/sde.sqlite.bz2
 echo "Decompressing bzip2..."
-bzip2 -d -v ./db/sde.sqlite.bz2
+bzip2 -d -v -f ./db/sde.sqlite.bz2
 echo "Done."
